61b506b0b97c88f85ef5c097dcf1feb7e683cb3e
lucafisc <lucafischer@fastmail.com>
2022-08-27 20:57:41 +0200

    Add components

---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+import background from "../images/bg-main-mobile.png";
+
+export default function Header() {
+  return (
+    <div className="header">
+      <img src={background} />
+    </div>
+  );
+}
