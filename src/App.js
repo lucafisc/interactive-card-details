4ae5419a6d50cba82f3bc072df5af2fe736a36b3
lucafisc <lucafischer@fastmail.com>
2022-08-31 10:34:00 +0200

    Add form validation

--- src/App.js
+++ src/App.js
@@ -30,9 +30,32 @@ function App() {
     e.preventDefault();
     let msgs = {};
     Object.keys(formData).forEach(function (key, index) {
-      msgs[key] = formData[key] ? false : "Can't be blank";
+      if (!formData[key]) {
+        msgs[key] = "Can't be blank";
+      } else if (key !== "name" && !onlyNumbers(formData[key])) {
+        msgs[key] = "Wrong format, numbers only";
+      } else if (key === "name" && !onlyLetters(formData[key])) {
+        msgs[key] = "Wrong format, characters only";
+      } else if (key === "name" && formData[key].length < 6) {
+        msgs[key] = "Name must be at least 6 characters long";
+      } else if (key === "number" && formData[key].length !== 16) {
+        msgs[key] = "Card number must have 16 digits";
+      } else if (
+        (key === "month" || key === "year") &&
+        formData[key].length !== 2
+      ) {
+        msgs[key] = "Date must have 2 digits";
+      } else if (key === "cvc" && formData[key].length !== 3) {
+        msgs[key] = "CVC must have 3 digits";
+      }
     });
     setErrMsg(msgs);
+  }
+  function onlyNumbers(str) {
+    return /^[0-9]+$/.test(str);
+  }
+  function onlyLetters(str) {
+    return /^[a-zA-Z\s]+$/.test(str);
   }
   return (
     <div className="App">
